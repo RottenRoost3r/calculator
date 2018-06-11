6ff4eebc5500a34e7ee9e1706bfbb87273192692
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-11 11:17:56 -0400

    first commit

---
+++ calculator.rb
@@ -0,0 +1,45 @@
+def add_function
+    print "What numbers?"
+    num1 = gets.chomp
+    num2 = gets.chomp
+    answer = num1.to_i + num2.to_i 
+    puts "The answer is #{answer}"
+end
+
+def subtract_function
+    print "What numbers?"
+    num1 = gets.chomp.to_i  
+    num2 = gets.chomp.to_i
+    answer = num1.to_i - num2.to_i
+    puts "The answer is #{answer}"
+end
+
+def multiply_function
+    print "What numbers?"
+    num1 = gets.chomp
+    num2 = gets.chomp
+    answer = num1.to_i * num2.to_i
+    
+    puts "The answer is #{answer}"
+end
+
+def divide_function
+    print "What numbers?"
+    num1 = gets.chomp
+    num2 = gets.chomp
+    answer = num1.to_i * num2.to_i
+    puts "The answer is #{answer}"
+end
+
+print "Would you like to [add] [subtract] [multiply] or [divide]?"
+Response = gets.chomp
+
+if Response == "add"
+    add_function
+elsif Response == "subtract"
+    subtract_function
+elsif Response == "multiply"
+    multiply_function
+elsif Response == "divide"
+    divide_function
+end
